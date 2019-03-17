cbf84d0db220e9628a60d1cad623b50728985a4f
BrendanZhang <ex.arc.zhang@gmail.com>
2019-03-18 05:38:24 +0800

    使用 chai spies 来 mock 数据测试点击事件

--- src/app.js
+++ src/app.js
@@ -16,7 +16,9 @@ new Vue({
 
 // 单元测试
 import chai from "chai"
+import spies from "chai-spies"
 const expect = chai.expect
+chai.use(spies)
 
 {
 	const Constructor = Vue.extend(Button)
@@ -76,9 +78,10 @@ const expect = chai.expect
 		}
 	})
 	vm.$mount()
-	vm.$on("click", function() {
-		console.log(1)
-	})
+	let spy = chai.spy(function() {})
+	vm.$on("click", spy)
+	// 希望这个函数被执行
 	let button = vm.$el
 	button.click()
+	expect(spy).to.have.been.called()
 }
