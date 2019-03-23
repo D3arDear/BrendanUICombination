57d4fdde9c9d409a7429f3504afbc8afa0ef2396
BrendanZhang <ex.arc.zhang@gmail.com>
2019-03-24 01:25:29 +0800

    add gutter and offset

--- src/col.vue
+++ src/col.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div
+    class="col"
+    :class="[span && `col-${span}`,offset && `offset-${offset}`]"
+    :style="{paddingLeft:gutter/2+ 'px',paddingRight:gutter/2 + 'px'}"
+  >
+    <div style="border:1px solid green;height:100%;">
+      <slot>1</slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "zealotCol",
+  data() {
+    return {
+      gutter: 0
+    };
+  },
   props: {
-    span: { type: [Number, String] }
+    span: {
+      type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
+    }
   }
 };
 </script>
@@ -16,16 +32,22 @@ export default {
 <style lang="scss" scoped>
 .col {
   height: 100px;
-  background: gray;
   width: 50%;
-  border: 1px solid red;
-
+  padding: 10px;
   $class-prefix: col-;
   // loops through 100 times
   @for $n from 1 through 24 {
     // for each $col_#{i}
     &.#{$class-prefix}#{$n} {
       width: ($n/24) * 100%;
+    }
+  }
+  $class-prefix: offset-;
+  // loops through 100 times
+  @for $n from 1 through 24 {
+    // for each $col_#{i}
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24) * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2 + 'px',marginRight:-gutter/2 + 'px'}">
     <slot></slot>
   </div>
 </template>
 <script>
+export default {
+  name: "zealotRow",
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  mounted() {
+    console.log(this.$children);
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter;
+    });
+  }
+};
 </script>
+
 <style lang="scss">
 .row {
   display: flex;
