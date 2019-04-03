9b265ccbfd30bb2dffadd8add5dae79b79a47019
BrendanZhang <ex.arc.zhang@gmail.com>
2019-04-04 06:07:39 +0800

    upgrade version

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import Button from "./src/button"
 import ButtonGroup from "./src/button-group"
 import Icon from "./src/icon"
+import Col from "./src/col"
+import Row from "./src/row"
+import Layout from "./src/layout"
+import Sider from "./src/sider"
+import Content from "./src/content"
+import Footer from "./src/footer"
+import Header from "./src/header"
+import Plugin from "./src/plugin"
+import Toast from "./src/toast"
 
-export { ButtonGroup, Button, Icon }
+export { ButtonGroup, Button, Icon, Col, Row, Layout, Sider, Content, Footer, Header, Plugin, Toast }
