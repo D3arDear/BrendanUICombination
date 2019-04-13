72160cd213b01f9caaeec5c6e998a64765491149
BrendanZhang <ex.arc.zhang@gmail.com>
2019-04-14 06:51:55 +0800

    popover 功能方面基本完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click.stop="xxx">
-    <div ref="contentWrapper" class="content-wrapper" v-show="visible" @click.stop>
+  <div class="popover" @click="onClick" ref="popover">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
@@ -15,35 +15,50 @@ export default {
     return { visible: false };
   },
   methods: {
-    xxx() {
-      this.visible = !this.visible;
-      console.log("切换 visible");
-      if (this.visible === true) {
-        this.$nextTick(() => {
-          document.body.appendChild(this.$refs.contentWrapper);
-          let {
-            width,
-            height,
-            top,
-            left
-          } = this.$refs.triggerWrapper.getBoundingClientRect();
-          this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
-          this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
-          let eventHandler = () => {
-            this.visible = false;
-            document.removeEventListener("click", eventHandler);
-          };
-          document.addEventListener("click", eventHandler);
-        });
-      } else {
-        console.log("隐藏 popover");
+    positionContent() {
+      document.body.appendChild(this.$refs.contentWrapper);
+      let {
+        width,
+        height,
+        top,
+        left
+      } = this.$refs.triggerWrapper.getBoundingClientRect();
+      this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
+      this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
+    },
+    onClickDocument(e) {
+      if (
+        this.$refs.popover &&
+        (this.$refs.popover === e.target ||
+          this.$refs.contentWrapper.contains(event.target))
+      ) {
+        return;
+      }
+      this.close();
+    },
+    close() {
+      this.visible = false;
+      document.removeEventListener("click", this.onClickDocument);
+    },
+    open() {
+      this.visible = true;
+      setTimeout(() => {
+        this.positionContent();
+        document.addEventListener("click", this.eventHandler);
+      });
+    },
+    onClick(event) {
+      console.log(event.target);
+      if (this.$refs.triggerWrapper.contains(event.target)) {
+        if (this.visible === true) {
+          this.close();
+        } else {
+          this.open();
+        }
       }
     }
   },
-  mounted() {
-    document.body.appendChild(this.$refs.contentWrapper);
-    console.log(this.$refs.triggerWrapper);
-  }
+  mounted() {}
 };
 </script>
 <style lang="scss" >
