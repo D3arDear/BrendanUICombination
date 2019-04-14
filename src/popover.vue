8fe927e2875e818da366a7171a8a93f53696b75b
BrendanZhang <ex.arc.zhang@gmail.com>
2019-04-15 00:16:14 +0800

    support four position of popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="popover" @click="onClick" ref="popover">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
+    <div
+      ref="contentWrapper"
+      class="content-wrapper"
+      :class="{[ `position-${position}` ]:true}"
+      v-if="visible"
+    >
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper" style="display:inline-block">
@@ -14,17 +19,37 @@ export default {
   data() {
     return { visible: false };
   },
+  props: {
+    position: {
+      type: String,
+      default: "top",
+      validator(value) {
+        return ["top", "bottom", "left", "right"].indexOf(value) >= 0;
+      }
+    }
+  },
   methods: {
     positionContent() {
-      document.body.appendChild(this.$refs.contentWrapper);
-      let {
-        width,
-        height,
-        top,
-        left
-      } = this.$refs.triggerWrapper.getBoundingClientRect();
-      this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
-      this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
+      const { contentWrapper, triggerWrapper } = this.$refs;
+      document.body.appendChild(contentWrapper);
+      let { width, height, top, left } = triggerWrapper.getBoundingClientRect();
+      if (this.position === "top") {
+        contentWrapper.style.left = left + window.scrollX + "px";
+        contentWrapper.style.top = top + window.scrollY + "px";
+      } else if (this.position === "bottom") {
+        contentWrapper.style.left = left + window.scrollX + "px";
+        contentWrapper.style.top = top + height + window.scrollY + "px";
+      } else if (this.position === "left") {
+        contentWrapper.style.left = left + window.scrollX + "px";
+        let { height: height2 } = contentWrapper.getBoundingClientRect();
+        contentWrapper.style.top =
+          top + window.scrollY + (height - height2) / 2 + "px";
+      } else if (this.position === "right") {
+        contentWrapper.style.left = left + window.scrollX + width + "px";
+        let { height: height2 } = contentWrapper.getBoundingClientRect();
+        contentWrapper.style.top =
+          top + window.scrollY + (height - height2) / 2 + "px";
+      }
     },
     onClickDocument(e) {
       if (
@@ -36,8 +61,8 @@ export default {
       }
       if (
         this.$refs.contentWrapper &&
-        (this.$refs.popover === e.target ||
-          this.$refs.popover.contains(e.target))
+        (this.$refs.contentWrapper === e.target ||
+          this.$refs.contentWrapper.contains(e.target))
       ) {
         return;
       }
@@ -80,11 +105,9 @@ $border-color: #ddd;
   position: absolute;
   border: 1px solid $border-color;
   filter: drop-shadow(0 0 2px $box-shadow-color);
-  transform: translateY(-100%);
-  margin-top: -10px;
-  padding: 0.5em 1em;
   max-width: 20em;
   word-break: break-all;
+  padding: 0.5em 1em;
   background: white;
   &::before,
   &::after {
@@ -95,12 +118,62 @@ $border-color: #ddd;
     height: 10px;
     background: $border-color;
     position: absolute;
-    top: calc(100%);
-    left: 10px;
   }
-  &::after {
-    background: white;
-    top: calc(100% - 1px);
+  &.position-top {
+    margin-top: -10px;
+    transform: translateY(-100%);
+    &::before,
+    &::after {
+      left: 10px;
+      top: 100%;
+    }
+    &::after {
+      background: white;
+      top: calc(100% - 1px);
+    }
+  }
+  &.position-left {
+    transform: translateX(-100%);
+    margin-left: -10px;
+    &::before,
+    &::after {
+      clip-path: polygon(0% 0%, 100% 30%, 0% 100%);
+      top: 5px;
+      left: 100%;
+      width: 10px;
+      height: 15px;
+    }
+    &::after {
+      background: white;
+      left: calc(100% - 1px);
+    }
+  }
+  &.position-right {
+    margin-left: 10px;
+    &::before,
+    &::after {
+      clip-path: polygon(100% 0%, 0% 30%, 100% 100%);
+      top: 5px;
+      right: 100%;
+      width: 10px;
+      height: 15px;
+    }
+    &::after {
+      background: white;
+      right: calc(100% - 1px);
+    }
+  }
+  &.position-bottom {
+    margin-top: 10px;
+    &::before,
+    &::after {
+      bottom: 100%;
+      clip-path: polygon(0% 100%, 30% 0%, 100% 100%);
+    }
+    &::after {
+      background: white;
+      bottom: calc(100% - 1px);
+    }
   }
 }
 </style>
