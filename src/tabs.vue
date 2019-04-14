d4799c9e4de36c208c73c10f2d221227bfe1c548
BrendanZhang <ex.arc.zhang@gmail.com>
2019-04-14 21:44:29 +0800

    重构 tabs.vue

--- src/tabs.vue
+++ src/tabs.vue
@@ -30,26 +30,34 @@ export default {
       eventBus: this.eventBus
     };
   },
-  mounted() {
-    if (this.$children.length === 0) {
-      console &&
-        console.warn &&
-        console.warn(
-          "tabs没有子组件,应该是tabs-head和tabs-nav,但你没有写子组件"
-        );
-    }
-    this.$children.forEach(vm => {
-      if (vm.$options.name === "zealotTabs-head") {
-        vm.$children.forEach(childVm => {
-          if (
-            childVm.$options.name === "zealotTabs-item" &&
-            childVm.name === this.selected
-          ) {
-            this.eventBus.$emit("update:selected", this.selected, childVm);
-          }
-        });
+  methods: {
+    checkChildren() {
+      if (this.$children.length === 0) {
+        console &&
+          console.warn &&
+          console.warn(
+            "tabs没有子组件,应该是tabs-head和tabs-nav,但你没有写子组件"
+          );
       }
-    });
+    },
+    selectTab() {
+      this.$children.forEach(vm => {
+        if (vm.$options.name === "zealotTabs-head") {
+          vm.$children.forEach(childVm => {
+            if (
+              childVm.$options.name === "zealotTabs-item" &&
+              childVm.name === this.selected
+            ) {
+              this.eventBus.$emit("update:selected", this.selected, childVm);
+            }
+          });
+        }
+      });
+    }
+  },
+  mounted() {
+    this.checkChildren();
+    this.selectTab();
   }
 };
 </script>
