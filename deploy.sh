024bfb161bdc2e49c497f40034bdc6129dd0937e
BrendanZhang <ex.arc.zhang@gmail.com>
2019-04-19 20:57:12 +0800

    初步完成文档框架

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
-set -e
+#set -e
 
 # 生成静态文件
-npm run docs:build
-
+yarn docs:build
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd ./docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,21 @@ module.exports = {
 	base: '/BrendanUICombination/',
 	themeConfig: {
 		nav: [{ text: '主页', link: '/' }],
-		sidebar: [{ title: '入门', children: ['/install/', '/get-started/'] }, { title: '组件', children: ['/components/button'] }],
+		sidebar: [
+			{ title: '入门', children: ['/install/', '/get-started/'] },
+			{
+				title: '组件',
+				children: [
+					'/components/button',
+					'/components/tabs',
+					'/components/input',
+					'/components/grid',
+					'/components/layout',
+					'/components/toast',
+					'/components/popover',
+				],
+			},
+		],
 	},
 	title: 'Zealot-UI',
 	description: '一个轻量的 UI 框架',
