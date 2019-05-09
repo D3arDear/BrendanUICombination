bb78ab7e1dfeaff81f03e9a06586319a42f8b13a
BrendanZhang <ex.arc.zhang@gmail.com>
2019-05-10 05:26:12 +0800

    nav 基本结构确定

--- src/demo.vue
+++ src/demo.vue
@@ -1,142 +1,26 @@
 <template>
   <div>
-    <div class="cascader-wrapper">
-      <z-cascader
-        :source.sync="source"
-        popover-height="200px"
-        :selected.sync="selectedSync"
-        :load-data="loadData"
-      ></z-cascader>
-    </div>
-    <div class="cascader-wrapper">
-      <z-cascader
-        :source.sync="source"
-        popover-height="200px"
-        :selected.sync="selectedSync"
-        :load-data="loadData"
-      ></z-cascader>
-      <z-button icon="setting">默认样式</z-button>
-    </div>
-    <div class="wrapper">
-      <z-slides :selected.sync="selected" height="300px">
-        <z-slides-item class="box" name="1">
-          <div class="slide-content">1</div>
-        </z-slides-item>
-        <z-slides-item class="box" name="2">
-          <div class="slide-content">2</div>
-        </z-slides-item>
-        <z-slides-item class="box" name="3">
-          <div class="slide-content">3</div>
-        </z-slides-item>
-      </z-slides>
-    </div>
+    <z-nav :selected.sync="selected">
+      <z-nav-item name="home">首页</z-nav-item>
+      <z-nav-item name="about">关于</z-nav-item>
+      <z-nav-item name="hire">招聘</z-nav-item>
+    </z-nav>
   </div>
 </template>
 <script>
-import Button from "./button/button";
-import SlidesItem from "./slides/slides-item";
-import Cascader from "./cascader/cascader";
-import Slides from "./slides/slides";
-import db from "../tests/fixture/db";
-import { removeListener } from "./click-outside";
-
-function ajax(parentId = 0) {
-  return new Promise((success, fail) => {
-    setTimeout(() => {
-      let result = db.filter(item => item.parent_id === parentId);
-      db.filter(item => item.parent_id == parentId);
-      result.forEach(node => {
-        if (db.filter(item => item.parent_id === node.id).length > 0) {
-          node.isLeaf = false;
-        } else {
-          node.isLeaf = true;
-        }
-      });
-      success(result);
-    }, 300);
-  });
-}
+import ZNav from "./nav/nav";
+import ZNavItem from "./nav/nav-item";
+import ZSubNav from "./nav/sub-nav";
 export default {
   name: "demo",
-  components: {
-    "z-cascader": Cascader,
-    "z-slides": Slides,
-    "z-slides-item": SlidesItem,
-    "z-button": Button
-  },
+  components: { ZNav, ZNavItem, ZSubNav },
   data() {
     return {
-      selected: undefined,
-      source: [],
-      selectedSync: []
+      selected: ["home"]
     };
   },
-  created() {
-    // let n = 1;
-    // setInterval(() => {
-    //   if (n === 4) {
-    //     n = 1;
-    //   }
-    //   this.selected = n.toString();
-    //   n++;
-    // }, 3000);
-    ajax(0).then(result => {
-      this.source = result;
-    });
-  },
-  destroyed() {
-    removeListener();
-  },
-  methods: {
-    loadData(node, fn) {
-      let id = node.id;
-      ajax(id).then(result => {
-        fn(result); // 把cascader传给我的函数调用一下
-      });
-    },
-    xxx() {
-      ajax(this.selectedSync[0].id).then(result => {
-        let lastLevelSelected = this.source.filter(
-          item => item.id === this.selectedSync[0].id
-        )[0];
-        //lastLevelSelected.children = result;
-        this.$set(lastLevelSelected, "children", result);
-      });
-    }
-  }
+  created() {}
 };
 </script>
-<style lang="scss">
-.box {
-  height: 350px;
-  background: #ddd;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  font-size: 30px;
-}
-img {
-  max-width: 100%;
-}
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-html {
-  --font-size: 14px;
-}
-body {
-  font-size: var(--font-size);
-}
-.slide-content {
-  width: 100%;
-  height: 400px;
-  background: #ddd;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid red;
-}
+<style lang="scss" scoped>
 </style>
---
+++ src/nav/nav-item.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div class="z-nav-item" :class="{selected}" @click="onClick">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ZealotNavItem",
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      selected: false
+    };
+  },
+  methods: {
+    onClick() {
+      this.$emit("add:selected", this.name);
+    }
+  },
+  mounted() {}
+};
+</script>
+
+<style lang="scss" scoped>
+.z-nav-item {
+  padding: 10px 20px;
+  &.selected {
+    background: red;
+  }
+}
+</style>
