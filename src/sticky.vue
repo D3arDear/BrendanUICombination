d832012922a155e80f741ff1fd1c285f2084445d
BrendanZhang <ex.arc.zhang@gmail.com>
2019-06-12 02:44:20 +0800

    init sticky

---
+++ src/sticky.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="zealot-sticky">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "zealotSticky",
+  props: {},
+  mounted() {},
+  created() {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > this.top()) {
+        console.log("滚过了元素的底部");
+      } else {
+        console.log("没有滚过元素的底部");
+      }
+    });
+  },
+  methods: {
+    top() {
+      let { top } = this.$refs.wrapper.getBoundingClientRect();
+      return top + window.scrollY;
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.zealot-sticky {
+  display: flex;
+}
+</style>
