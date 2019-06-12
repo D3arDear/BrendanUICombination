74a2049c4d94da4658a47446ea52597a5e9b04ec
BrendanZhang <ex.arc.zhang@gmail.com>
2019-06-13 02:15:19 +0800

    完成sticky功能

--- src/sticky.vue
+++ src/sticky.vue
@@ -1,32 +1,67 @@
 <template>
-  <div class="zealot-sticky">
-    <slot></slot>
+  <div class="zealot-sticky-wrapper" ref="wrapper" :style="{height}">
+    <div class="zealot-sticky" :class="classes" :style="{left,width}">
+      <slot></slot>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "zealotSticky",
   props: {},
-  mounted() {},
-  created() {
+  data() {
+    return {
+      sticky: false,
+      left: undefined,
+      width: undefined,
+      height: undefined
+    };
+  },
+  mounted() {
+    let { top } = this.top();
     window.addEventListener("scroll", () => {
-      if (window.scrollY > this.top()) {
+      if (window.scrollY > top) {
         console.log("滚过了元素的底部");
+        let {
+          height,
+          left,
+          width
+        } = this.$refs.wrapper.getBoundingClientRect();
+        console.log(height, left, width);
+        this.height = height + "px";
+        this.left = left + "px";
+        this.width = width + "px";
+        this.sticky = true;
       } else {
         console.log("没有滚过元素的底部");
+        this.sticky = false;
       }
     });
   },
+  created() {},
+  watch() {},
   methods: {
     top() {
       let { top } = this.$refs.wrapper.getBoundingClientRect();
-      return top + window.scrollY;
+      return { top: top + window.scrollY };
+    }
+  },
+  computed: {
+    classes() {
+      return {
+        sticky: this.sticky
+      };
     }
   }
 };
 </script>
 <style lang="scss" scoped>
 .zealot-sticky {
-  display: flex;
+  &.sticky {
+    position: fixed;
+    top: 0;
+    // left: 0;
+    // width: 100%;
+  }
 }
 </style>
