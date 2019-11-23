5cc5efc3e31c949f937715396093a79b152d62ee
Brendan <ex.arc.zhang@gmail.com>
2019-11-24 04:12:40 +0800

    tabs.spec.js selected disabled

--- tests/unit/tabs.spec.js
+++ tests/unit/tabs.spec.js
@@ -21,28 +21,28 @@ describe("Tabs", () => {
   it("存在.", () => {
     expect(Tabs).to.exist;
   });
-  // it("接受 selected", (done) => {
-  // 	const div = document.createElement("div")
-  // 	document.body.appendChild(div)
-  // 	div.innerHTML = `<z-tabs selected="pc">
-  //      <z-tabs-head>
-  // 				<z-tabs-item name="pc"> 电脑 </z-tabs-item>
-  // 				<z-tabs-item name="phone"> 手机 </z-tabs-item>
-  // 				<z-tabs-item name="camera"> 相机 </z-tabs-item>
-  // 			</z-tabs-head>
-  // 			<z-tabs-body>
-  // 				<z-tabs-pane name="pc"> 电脑相关 </z-tabs-pane>
-  // 				<z-tabs-pane name="phone"> 手机相关 </z-tabs-pane>
-  // 				<z-tabs-pane name="camera"> 相机相关 </z-tabs-pane>
-  // 			</z-tabs-body>
-  //     </z-tabs>`
-  // 	let vm = new Vue({
-  // 		el: div,
-  // 	}).$mount(div)
-  // 	vm.$nextTick(() => {
-  // 		let x = vm.$el.querySelector(`.tabs-item[data-name='pc']`)
-  // 		expect(x.classList.contains("active")).to.be.true
-  // 		done()
-  // 	})
-  // })
+  xit("接受 selected", done => {
+    const div = document.createElement("div");
+    document.body.appendChild(div);
+    div.innerHTML = `<z-tabs selected="pc">
+       <z-tabs-head>
+  				<z-tabs-item name="pc"> 电脑 </z-tabs-item>
+  				<z-tabs-item name="phone"> 手机 </z-tabs-item>
+  				<z-tabs-item name="camera"> 相机 </z-tabs-item>
+  			</z-tabs-head>
+  			<z-tabs-body>
+  				<z-tabs-pane name="pc"> 电脑相关 </z-tabs-pane>
+  				<z-tabs-pane name="phone"> 手机相关 </z-tabs-pane>
+  				<z-tabs-pane name="camera"> 相机相关 </z-tabs-pane>
+  			</z-tabs-body>
+      </z-tabs>`;
+    let vm = new Vue({
+      el: div
+    }).$mount(div);
+    vm.$nextTick(() => {
+      let x = vm.$el.querySelector(`.tabs-item[data-name='pc']`);
+      expect(x.classList.contains("active")).to.be.true;
+      done();
+    });
+  });
 });
