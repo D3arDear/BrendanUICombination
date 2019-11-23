c3627d80e837b5afd955d846918fc70c293cdcbe
Brendan <ex.arc.zhang@gmail.com>
2019-11-24 01:30:57 +0800

    travis config

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-import Button from './src/button'
-import ButtonGroup from './src/button-group'
-import Icon from './src/icon'
-import Col from './src/col'
-import Row from './src/row'
-import Layout from './src/layout'
-import Sider from './src/sider'
-import Content from './src/content'
-import Footer from './src/footer'
-import Header from './src/header'
-import Plugin from './src/plugin'
-import Toast from './src/toast'
-import Tabs from './src/tabs'
-import TabsHead from './src/tabs-head'
-import TabsBody from './src/tabs-body'
-import TabsItem from './src/tabs-item'
-import TabsPane from './src/tabs-pane'
-import Popover from './src/popover'
-import Collapse from './src/collapse'
-import CollapseItem from './src/collapse-item'
+import Button from "./src/button";
+import ButtonGroup from "./src/button-group";
+import Icon from "./src/icon";
+import Col from "./src/col";
+import Row from "./src/row";
+import Layout from "./src/layout";
+import Sider from "./src/sider";
+import Content from "./src/content";
+import Footer from "./src/footer";
+import Header from "./src/header";
+import Plugin from "./src/plugin";
+import Toast from "./src/toast";
+import Tabs from "./src/tabs";
+import TabsHead from "./src/tabs-head";
+import TabsBody from "./src/tabs-body";
+import TabsItem from "./src/tabs-item";
+import TabsPane from "./src/tabs-pane";
+import Popover from "./src/popover";
+import Collapse from "./src/collapse";
+import CollapseItem from "./src/collapse-item";
+import Sticky from "./src/sticky";
 
-export { ButtonGroup, Button, Icon, Col, Row, Layout, Sider, Content, Footer, Header, Plugin, Toast, Tabs, TabsHead, TabsBody, TabsItem, TabsPane, Popover, Collapse, CollapseItem }
+export {
+  ButtonGroup,
+  Button,
+  Icon,
+  Col,
+  Row,
+  Layout,
+  Sider,
+  Content,
+  Footer,
+  Header,
+  Plugin,
+  Toast,
+  Tabs,
+  TabsHead,
+  TabsBody,
+  TabsItem,
+  TabsPane,
+  Popover,
+  Collapse,
+  CollapseItem,
+  Sticky
+};
